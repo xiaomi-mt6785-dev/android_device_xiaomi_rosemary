4c9f22bf75edeb950e1123b495b8915696ed1937
bengris32 <bengris32@protonmail.ch>
2022-09-16 21:44:36 +0100

    rosemary: Build Thermal 2.0 interface
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Ib25eca2ef8f800e79c751c608cb0f307a60314bc

--- device.mk
+++ device.mk
@@ -296,6 +296,9 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl
 
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic

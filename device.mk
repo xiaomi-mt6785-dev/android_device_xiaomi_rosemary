4b6dea31646a59d8758136aa1aa1753bd3b06277
Danny Baumann <dannybaumann@web.de>
2022-09-18 23:01:43 +0100

    rosemary: Enable call recording
    Change-Id: I1b4478b330b13cf5d0fe1ed0d40f43e4b3825f33

--- device.mk
+++ device.mk
@@ -230,7 +230,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     WifiOverlay \

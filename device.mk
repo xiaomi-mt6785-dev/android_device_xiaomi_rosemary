c3a22a6b0a990dff4b7ba78bc2928b07a5a59b27
SamarV-121 <samarvispute121@pm.me>
2022-09-24 16:04:25 +0100

    rosemary: Build and rename audio service HAL
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- device.mk
+++ device.mk
@@ -40,7 +40,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.service \
+    android.hardware.audio.service.mediatek \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.audio@2.1-impl

d2fdc245cdb5ed495a5190c67d1ed80efcef732b
bengris32 <bengris32@protonmail.ch>
2022-09-26 15:30:40 +0100

    rosemary: Convert overlays into Runtime Resource Overlays
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I9105bbd5b4cb183b9eed02d15d4ca3134fbcb6d2

--- device.mk
+++ device.mk
@@ -233,18 +233,19 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_PACKAGES += \
-    WifiOverlay \
-    TetheringConfigOverlay \
-    CarrierConfigOverlayRosemary
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlayRosemary \
+    FrameworksResOverlayRosemary \
+    TelephonyOverlayRosemary \
+    TetheringConfigOverlayRosemary \
+    WifiOverlayRosemary
 
 # Power
 PRODUCT_PACKAGES += \

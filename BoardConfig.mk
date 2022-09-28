c9e6067d9e8f506abb12ea923ccd314c31f99504
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-09-28 19:17:19 +0100

    rosemary: Enable updateable APEX

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/xiaomi/rosemary
 
+# APEX
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 

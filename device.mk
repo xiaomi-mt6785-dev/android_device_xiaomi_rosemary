a064b72a0ea0a100bd5ff57b2651997c4f4b8ca4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-28 19:18:01 +0100

    rosemary: Enforce RRO for all overlays

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 

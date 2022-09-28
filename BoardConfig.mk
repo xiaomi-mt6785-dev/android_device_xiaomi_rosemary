6f87dd271c921ebb26f8e3733e79d9bd0866bba0
bengris32 <bengris32@protonmail.ch>
2022-09-28 19:08:17 +0100

    rosemary: Define Architecture flags
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,20 @@
 
 DEVICE_PATH := device/xiaomi/rosemary
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
 # Inherit the proprietary files
 include vendor/xiaomi/rosemary/BoardConfigVendor.mk

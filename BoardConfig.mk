f1976cb3e3f72d1a63dd990992b53cf0285a4a7e
Alexander Koskovich <akoskovich@pm.me>
2022-10-02 21:38:39 +0530

    rosemary: Move BT profiles props to vendor
     * Makes it GSI compatible
    
    Change-Id: I8f7caf241d508af59c16482e6185c42f23f8a460

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,7 +126,6 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery

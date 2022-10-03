91c23d1cb70376d63af66e8f408c2a53454f5f65
bengris32 <bengris32@protonmail.ch>
2022-10-03 20:01:09 +0100

    rosemary: Link audio modules against prebuilt `libalsautils.so` * Fixes USB DAC.
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I63b4ce60e89ffe04c0e7dd558e21f9258f7112c9

--- extract-files.sh
+++ extract-files.sh
@@ -57,6 +57,10 @@ function blob_fixup {
     case "$1" in
         vendor/lib*/hw/audio.primary.mt6785.so)
             "${PATCHELF}" --add-needed "libshim_audio.so" "${2}"
+            "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v30.so" "${2}"
+            ;;
+        vendor/lib*/hw/audio.usb.mt6785.so)
+            "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v30.so" "${2}"
             ;;
         vendor/lib64/libwifi-hal-mtk.so)
             "$PATCHELF" --set-soname libwifi-hal-mtk.so "${2}"

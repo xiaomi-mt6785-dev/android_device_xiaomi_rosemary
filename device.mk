56e35bf53eb15c547e0b25f79d064d52238890c0
danascape <danascape@gmail.com>
2022-12-04 12:54:56 +0100

    rosemary: Build soundtrigger 2.3
    Change-Id: I0f8743ac5266329b79022e9ad6392c9e9afd73b9

--- device.mk
+++ device.mk
@@ -43,7 +43,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service.mediatek \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.bluetooth.audio-impl
+    android.hardware.bluetooth.audio-impl \
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \

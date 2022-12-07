044c888f9d0fec10711af986080c771044b606f2
Daniel Rosenberg <drosen@google.com>
2022-12-07 12:23:23 +0100

    rosemary: Setup checkpoint_gc for /vendor
    Test: start checkpointing, observe that checkpointing happens
    Bug: 111020314
    Change-Id: Ic090ac2d0ca9a7dd5485f5ab2a967977cd3ab6b8

--- device.mk
+++ device.mk
@@ -34,6 +34,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script

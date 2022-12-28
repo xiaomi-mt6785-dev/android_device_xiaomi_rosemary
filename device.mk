9a67b9de6b8b63215c9e43a0dc24d3b340d8887f
Matsvei Niaverau <matvej2002@gmail.com>
2022-12-28 22:20:12 +0100

    rosemary: configs: Import rosemary powerhint
    * Imported from mt6768-common tree & adapted for rosemary
    
    Change-Id: I0d5a58bebdb85cc6c2645084b7472f50dc20f8e7

--- device.mk
+++ device.mk
@@ -335,6 +335,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     android.hardware.power@1.3.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Properties
 include $(LOCAL_PATH)/vendor_logtag.mk
 

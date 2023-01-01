3b4c1eb36d84d93ad16596b0e59d221349e9e222
Matsvei Niaverau <matvej2002@gmail.com>
2023-01-01 13:41:45 +0100

    rosemary: Define AAPT configuration
    Change-Id: Ic3a241c34c9d6437aebc7007a82487e5e42069b3

--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit virtual_ab_ota product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # A/B
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service \

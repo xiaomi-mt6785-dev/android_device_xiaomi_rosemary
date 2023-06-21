c4a4c66c1ae5560abf6dcd63c118bdebc9cda85d
Michael Bestas <mkbestas@lineageos.org>
2023-06-21 09:52:20 +0200

    rosemary: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- device.mk
+++ device.mk
@@ -248,6 +248,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.rosemary
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \

09d32045708529bbcd5dbb6ef2357a8551543b18
Sarthak Roy <sarthakroy2002@gmail.com>
2023-06-24 22:58:01 +0530

    rosemary: sepolicy: Fix property violations
    Change-Id: I736d314c2d9bb1444e542739a7b6b70c62395580

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,6 +161,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # SPL
 VENDOR_SECURITY_PATCH := 2021-10-01

be06ba6a45eab21946f5de0bd36689cc277f2bda
Yi-Yo Chiang <yochiang@google.com>
2023-09-04 15:07:07 +0100

    rosemary: Remove references to BOARD_BUILD_SYSTEM_ROOT_IMAGE
    This variable is always false or empty now.
    Remove it so we can mark it as deprecated later.
    
    Bug: 241346584
    Test: Presubmit
    Change-Id: I57cbe875f556bb853d4ea9b1d458c9c554c6bfa5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,7 +9,6 @@ DEVICE_PATH := device/xiaomi/rosemary
 # A/B
 AB_OTA_UPDATER := true
 BOARD_USES_RECOVERY_AS_BOOT := true
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 
 AB_OTA_PARTITIONS := \
     boot \

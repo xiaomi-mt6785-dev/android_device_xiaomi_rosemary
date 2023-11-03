967d552a2f8266d8b5875dcba6278e3516756b91
bengris32 <bengris32@protonmail.ch>
2023-11-03 10:19:17 +0100

    rosemary: Switch to AOSP audio service
    Change-Id: I933b66e3821562022eb837af91ad4039e22fb578
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -49,7 +49,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.service.mediatek \
+    android.hardware.audio.service \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.bluetooth.audio-impl \

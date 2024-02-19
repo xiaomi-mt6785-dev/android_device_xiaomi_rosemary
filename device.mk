b8b7b383cc3e4b895fbefaf38e4790024d615f6e
bengris32 <bengris32@protonmail.ch>
2024-02-19 12:02:32 +0100

    rosemary: Switch to common USB service
    Change-Id: Ie7bc896c51c435ddd7906f02409c3610386e9ea3
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -462,12 +462,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0.vendor \
-    android.hardware.usb@1.1.vendor \
-    android.hardware.usb@1.2.vendor \
-    android.hardware.usb@1.3.vendor \
-    android.hardware.usb.gadget@1.0.vendor \
-    android.hardware.usb.gadget@1.1.vendor
+    android.hardware.usb-service.mediatek
 
 # Vibrator
 PRODUCT_PACKAGES += \

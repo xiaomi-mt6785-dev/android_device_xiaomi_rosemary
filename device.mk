2ee249af7cf2c83332f529993e55e281723785e4
bengris32 <bengris32@protonmail.ch>
2024-03-26 15:09:16 +0100

    rosemary: Build common MediaTek Gadget HAL
    Change-Id: I4951a30cee215aab57ce44ab86ef9ee2b60fb360
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -463,7 +463,7 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
-    android.hardware.usb.gadget-service.rosemary
+    android.hardware.usb.gadget-service.mediatek
 
 # Vibrator
 PRODUCT_PACKAGES += \

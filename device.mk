a4327c6d08885b8cd35c9c49c58325fbd567c974
Matsvei Niaverau <matvej2002@gmail.com>
2026-01-10 12:09:55 +0100

    rosemary: Initial powerhint for MT6785
    Change-Id: I0cbb713a33c4d083d26984062a3eac8f374816dc

--- device.mk
+++ device.mk
@@ -238,6 +238,9 @@ PRODUCT_PACKAGES += \
     libmtkperf_client_vendor \
     libmtkperf_client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # PowerOffAlarm
 PRODUCT_PACKAGES += \
     PowerOffAlarm

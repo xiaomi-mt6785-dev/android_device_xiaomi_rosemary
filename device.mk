570a09580e93adccf86d7521ae627d9c1ad4235a
Mashopy <eliasgheeraert@gmail.com>
2026-07-06 18:38:41 +0200

    rosemary: Declare wifi HAL wrapper struct is older than U QPR2
    Change-Id: I370e553846e493445a814edd1a7842cda75fb954

--- device.mk
+++ device.mk
@@ -357,6 +357,8 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.rosemary
 
 # Wi-Fi
+$(call soong_config_set_bool,mediatek_wifi_hal,use_pre_u_qpr2_struct,true)
+
 PRODUCT_PACKAGES += \
     wpa_supplicant \
     hostapd \

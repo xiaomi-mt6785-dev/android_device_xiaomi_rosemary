4a53cab3d6967871f288e7cebb07fb0312602082
Ben Grisdale <bengris32@protonmail.ch>
2026-07-08 22:26:37 +0200

    rosemary: Build MediaTek health AIDL service
    Change-Id: I94dd6476565b618fc447027e8b5d5f7645c35874
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -137,9 +137,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.mediatek \
+    android.hardware.health-service.mediatek-recovery
 
 # Hotword Enrollment
 PRODUCT_COPY_FILES += \

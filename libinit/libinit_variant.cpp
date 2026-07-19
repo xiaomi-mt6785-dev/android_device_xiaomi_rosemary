4d778b7de805932586614272ede1b81ed8f819ce
probaero <87478728+probaero@users.noreply.github.com>
2026-07-19 22:51:29 +0700

    rosemary: libinit: Don't set per SKU build fingerprint and description
    Change-Id: I073cf646bf558d68c6e769063e2767d8d0e0938a

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -40,10 +40,6 @@ void set_variant_props(const variant_info_t variant) {
 
     if (access("/system/bin/recovery", F_OK) != 0) {
         property_override("bluetooth.device.default_name", variant.marketname, true);
-        set_ro_build_prop("fingerprint", variant.build_fingerprint);
-        property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
-
-        property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
     }
 
     property_override("ro.boot.hardware.sku", variant.device);
